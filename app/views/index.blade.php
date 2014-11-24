a157e5b5d075c345100ad47d3a98c5b6511b4baf
Md Imran <shellprog@gmail.com>
2014-11-24 22:40:36 +0530

    Initial

---
+++ app/views/index.blade.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<!-- saved from url=(0031)http://www.williamhharding.com/ -->
+<html lang="en" class="csstransforms csstransforms3d csstransitions js cssanimations csstransitions"><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta charset="utf-8">
+    <title>Law Offices of William H. Harding</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- start: CSS -->
+    <link href="/res/css/bootstrap.css" rel="stylesheet">
+    <link href="/res/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="/res/css/style.css" rel="stylesheet">
+	<link href="/res/css/parallax-slider.css" rel="stylesheet">
+	<link href="/res/css/fonts.css" rel="stylesheet">
+	<!-- end: CSS -->
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  <body style="transition:top 1s ease;-webkit-transition:top 1s ease;-moz-transition:top 1s ease;">
+  <!--start: Header -->
+	<header>
+		
+		<!--start: Container -->
+		<div class="container">
+			
+			<!--start: Row -->
+			<div class="row">
+					
+				<!--start: Logo -->
+				<div class="logo span3">
+						
+					<img src="/res/img/WHH_logo_full.png">
+						
+				</div>
+				<!--end: Logo -->
+					
+				<!--start: Navigation -->
+				<div class="span9">
+					
+					<div class="navbar navbar-inverse">
+			    		<div class="navbar-inner">
+			          		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			            		<span class="icon-bar"></span>
+			            		<span class="icon-bar"></span>
+			            		<span class="icon-bar"></span>
+			          		</a>
+			          		<div class="nav-collapse collapse">
+			            		<ul class="nav">
+									<li class="active dropdown">
+			                			<a href="/">Home</a>
+			              			</li>
+			              			<li><a href="/">About</a></li>
+									<li class="dropdown">
+			                			<a href="/" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a>
+			                			<ul class="dropdown-menu">
+			                  				<li><a href="/">Personal Injury</a></li>
+											<li><a href="/">Criminal Law</a></li>
+											<li><a href="/">Wrongful Death</a></li>
+			                			</ul>
+			              			</li>
+			              			<li><a href="/">Resources</a></li>
+			              			<li><a href="/">FAQ</a></li>
+			              			<li><a href="/">Contact Us</a></li>
+			            		</ul>
+			          		</div>
+			        	</div>
+			      	</div>
+					
+				</div>	
+				<!--end: Navigation -->
+					
+			</div>
+			<!--end: Row -->
+			
+			<div class="row">
+				<div class="span9">
+					<h3>"If you've got a phone, you've got a lawyer"</h3>
+				</div>
+				<div class="span3" style="text-align: right">
+					<h1>704.509.3090</h1>
+				</div>
+			</div>
+			
+		</div>
+		<!--end: Container-->			
+			
+	</header>
+	<!--end: Header-->
+	
+	<!-- start: Slider -->
+	<div class="slider-wrapper">
+
+		<div id="da-slider" class="da-slider" style="background-position: 500% 0%;">
+			<div class="da-slide da-slide-toleft">
+				<h2>No upfront fees</h2>
+				<p>For any personal injury case, you don't pay anything unless we win.</p>
+				<a href="/" class="da-link">Learn more</a>
+			</div>
+			<div class="da-slide da-slide-fromright da-slide-current">
+				<h2>Easy access</h2>
+				<p>No matter the situation, speak directly to your attorney 24 hours a day, 7 days a week.</p>
+				<a href="/" class="da-link">Learn more</a>
+			</div>
+			<div class="da-slide da-slide-toleft">
+				<h2>Guard your rights</h2>
+				<p>We are backed by 18 years of experience providing first class legal services.</p>
+				<a href="/" class="da-link">Learn more</a>
+			</div>
+			<nav class="da-arrows">
+				<span class="da-arrows-prev"></span>
+				<span class="da-arrows-next"></span>
+			</nav>
+		<nav class="da-dots"><span class=""></span><span class="da-dots-current"></span><span class=""></span></nav></div>
+		
+	</div>
+	<!-- end: Slider -->
+			
+	<!--start: Wrapper-->
+	<div id="wrapper">
+				
+		<!--start: Container -->
+    	<div class="container">
+			
+			<!-- start: Row -->
+      		<div class="row">
+				<div class="span9">
+					<div class="title"><h3>Areas of Expertise</h3></div>
+					
+					<!-- start: Row -->
+		      		<div class="row">
+						<div class="span3">
+							<div class="picture">
+								<a href="/" rel="image" title="Personal Injury">
+									<img src="/res/img/accident.png" class="hidden-phone">
+								</a>
+							</div>
+							<div class="item-description">
+								<h4><a href="/">Personal Injury</a></h4>
+								<p>Throughout the years, we have tried and won numerous cases and recovered millions of dollars for our clients. We represent auto accidents, slip and fall, medical malpractice, and workers compensation cases - a fee is collected only after you win.</p>
+							</div>
+        				</div>
+						<div class="span3">
+							<div class="picture">
+								<a href="/">
+									<img src="/res/img/gavel.png" class="hidden-phone">
+								</a>
+							</div>
+							<div class="item-description">
+								<h4><a href="/">Criminal Law</a></h4>
+								<p>DUIs, speeding tickets, possession, driving with a suspended license, and driving privilege restoration. We also handle misdemeanors and felony cases, including drug offenses and grand larceny. No matter the charge, you can count on us to defend your rights.</p>
+							</div>
+		        		</div>
+						<div class="span3">
+							<div class="picture">
+								<a href="/" rel="image" title="Family Law">
+									<img src="/res/img/penpaper.png" class="hidden-phone">
+								</a>
+							</div>
+							<div class="item-description">
+								<h4><a href="/">Wrongful Death</a></h4>
+								<p>Wrongful death cases require experience, knowledge and compassion for each client's difficult situation. At the Law Offices of William H. Harding, you will feel confident knowing your case is in capable hands.</p>
+							</div>
+
+						</div>
+					
+        			</div>
+					<!-- end: Row -->
+
+				</div>
+
+        		<div class="span3">
+					
+					<!-- start: Testimonials-->
+
+					<div class="testimonial-container">
+
+						<div class="title"><h3>What Clients are Saying</h3></div>
+
+							<div class="testimonials-carousel" data-autorotate="3000" role="application" aria-live="polite" style="overflow: hidden; width: 100%;">	<a href="http://www.williamhharding.com/#carousel-1-0" class="mr-rotato-next mr-rotato-disabled" aria-disabled="true"></a>	<a href="http://www.williamhharding.com/#carousel-1-0" class="mr-rotato-prev"></a>
+
+								<ul class="carousel" id="carousel-1-0" aria-activedescendant="carousel-1-0-slide1" style="margin-left: -100%; float: left; width: 200%; -webkit-transition: margin-left 0.3s ease; transition: margin-left 0.3s ease;">
+
+									<li class="testimonial" role="tabpanel document" id="carousel-1-0-slide0" aria-hidden="true" style="float: left; width: 50%;">
+										<div class="testimonials">His attention to my concerns and worries exceeded my expectations. I was quite amazed with his professional approach, time management and attention to detail. I recommend William Harding to anyone looking for a Trustworthy, VERY knowledgeable Attorney for Personal Injury, CAR or AUTO accident cases. A Five-Star attorney with a "Hometown" touch!</div>
+										<div class="testimonials-bg"></div>
+									</li>
+
+									<li class="testimonial mr-rotato-active-slide" role="tabpanel document" id="carousel-1-0-slide1" aria-hidden="false" style="float: left; width: 50%;">
+										<div class="testimonials">I had a car injury case, where I got hit from behind in a car accident and I was paralyzed for a short period of time and had major surgeries on the disc in Virginia. A friend referred me to Attorney William Harding. He is not only the most wonderful attorney, but also most wonderful human being! He was really honest whether it was good or bad, and very prompt.</div>
+										<div class="testimonials-author">Candace S.</div>
+										<div class="testimonials-bg"></div>
+									</li>
+								</ul>
+
+							</div>
+
+						</div>
+
+					<!-- end: Testimonials-->
+					
+        		</div>
+
+      		</div>
+			<!-- end: Row -->
+		</div>
+		<!--end: Container-->
+	
+	</div>
+	<!-- end: Wrapper  -->	
+
+	<!-- start: Footer -->
+	<div id="footer">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+			
+				<!-- start: Request Form -->
+				<div class="span6">
+					<form id="newsletter">
+						<h3>Request a Consultation</h3>
+						<input type="text" id="name" placeholder="Name">
+						<input type="text" id="phone_number" placeholder="Phone">
+						<input type="text" id="email_address" placeholder="Email">
+						<input type="text" id="comments" placeholder="Comments"><br>
+						<input type="submit" id="contact_submit" value="submit">
+					</form>
+				</div>
+				<!-- end: Request Form -->
+
+				<!-- start: Photo Stream -->
+				<div class="span3">
+					
+					<h3>Office Location</h3>
+					<div id="small-map-container"><a href="/"></a></div>
+					<iframe id="small-map" width="210" height="210" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="/res/embed.html"></iframe>
+					
+				</div>
+				<!-- end: Photo Stream -->
+				
+				<div class="span3">
+					<h3>Contact Info</h3>
+					<address style="font-size: 16px">
+						<strong>Law Offices of William H. Harding</strong><br>
+						9115 Harris Corners Parkway<br>
+						Suite 220<br>
+						Charlotte, NC 28269<br>
+						<h3><strong>Office: </strong><a href="tel:1-704-509-3090">704.509.3090</a><br>
+						<strong>Fax: </strong><a href="tel:1-704-919-5620">704.919.5620</a>
+						</h3>
+					</address>
+					<!-- start: Follow Us -->
+					<h3>Follow Us!</h3>
+					<ul class="social-grid">
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-twitter">
+											<a href="http://twitter.com/"></a>
+										</div>
+										<div class="social-info-back social-twitter-hover">
+											<a href="http://twitter.com/"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-facebook">
+											<a href="http://facebook.com/"></a>
+										</div>
+										<div class="social-info-back social-facebook-hover">
+											<a href="http://facebook.com/"></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+					<!-- end: Follow Us -->				
+				</div>
+				
+			</div>
+			<!-- end: Row -->	
+			
+		</div>
+		<!-- end: Container  -->
+
+	</div>
+	<!-- end: Footer -->
+	
+	<!-- start: Footer Menu -->
+	<div id="footer-menu" class="hidden-tablet hidden-phone">
+
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+
+				<!-- start: Footer Menu Links-->
+				<div class="span11">
+					<div id="footer-menu-links">
+						<ul id="footer-nav">
+							<li><a href="/">Home</a></li>
+							<li><a href="/">About</a></li>
+							<li><a href="/">Services</a></li>
+							<li><a href="/">Resources</a></li>
+							<li><a href="/">Contact</a></li>
+							<li><a href="/">FAQ</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- end: Footer Menu Links-->
+
+				<!-- start: Footer Menu Back To Top -->
+				<div class="span1">
+						
+					<div id="footer-menu-back-to-top">
+						<a href="/"></a>
+					</div>
+				
+				</div>
+				<!-- end: Footer Menu Back To Top -->
+			
+			</div>
+			<!-- end: Row -->
+			
+		</div>
+		<!-- end: Container  -->	
+
+	</div>	
+	<!-- end: Footer Menu -->
+
+
+
+<!-- Placed at the end of the document so the pages load faster -->
+
+	<!-- start: Java Script -->
+<!--    ================================================== -->
+  <script src="/res/js/lib/jquery-1.8.2.js"></script> 
+    <script src="/res/js/lib/smart/bootstrap.js"></script>
+	<script src="/res/js/lib/smart/isotope.js"></script>
+	<script src="/res/js/lib/smart/flexslider.js"></script>
+	<script src="/res/js/lib/smart/carousel.js"></script>
+	<script src="/res/js/lib/smart/jquery.cslider.js"></script>
+	<script src="/res/js/lib/smart/slider.js"></script>
+	<script src="/res/js/lib/smart/fancybox.js"></script>
+	<script defer="defer" src="/res/js/lib/smart/custom.js"></script>
+    <script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+          $('#myCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-40579653-1', 'williamhharding.com');
+	  ga('send', 'pageview');
+	
+	</script>
+  
+
+</body></html>
